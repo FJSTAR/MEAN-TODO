0d513f0342f526e0ce7247d7c5d0d38a755ffa19
FJSTAR <46565902+FJSTAR@users.noreply.github.com>
2020-07-26 17:44:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var path = require('path')
 var bodyParser = require('body-parser')
 
-var index = require('./routes/index')
+// var index = require('./routes/index')
 var tasks = require('./routes/tasks')
 var cors = require('cors')
 
